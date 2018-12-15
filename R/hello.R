11ecc6d5f8e7b2b51552ce8dc83df227a49012bd
karacitir <itir.karac@gmail.com>
2018-12-15 15:33:06 +0200

    Dataset data_experiment added to tha package

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,11 @@
-# Hello, world!
+#' Hello, world!
 #
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
+#' This is an example function named 'hello'
+#' which prints 'Hello, world!'.
+#'
+#' @export
+#'
+#' @examples hello()
 hello <- function() {
-  print("Hello, world!")
+  print("Hello World!")
 }
