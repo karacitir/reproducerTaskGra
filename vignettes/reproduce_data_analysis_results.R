a4b9e7193331ad78158f1ab944f46b9604439544
karacitir <itir.karac@gmail.com>
2019-01-09 16:41:00 +0200

    Vignette update

--- vignettes/reproduce_data_analysis_results.R
+++ vignettes/reproduce_data_analysis_results.R
@@ -1,25 +1,38 @@
 ## ----setup, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>",
-  fig.align = "center"
+collapse = TRUE,
+comment = "#>",
+fig.align = "center"
 )
 
 ## ------------------------------------------------------------------------
 library(reproducerTaskGra)
 
-## ----participant_demographics, fig.cap = "Figure 1"----------------------
-  reproduce_barplot_participants()
+## ----participant_demographics, fig.cap = "Fig. 1: Summary of demographic survey for participants"----
+reproduce_barplot_participants()
 
 ## ----descriptive_stats_COR-----------------------------------------------
 knitr::kable(reproduce_descriptive_stats_CORRECTNESS())
 
-## ---- fig.cap = "Figure X"-----------------------------------------------
-  reproduce_violin_plot_CORRECTNESS()
+## ---- fig.cap = "Fig. 2. Violin plots for CORRECTNESS at both levels of Task Description Granularity. Data points are indicated as dots grouped into bins of width 5. Horizontal lines within the plots indicate 0.25, 0.50 and 0.75 quantiles."----
+reproduce_violin_plot_CORRECTNESS()
+
+## ---- fig.width = 8, fig.cap = "Fig. 3. Boxplots for CORRECTNESS shown separately on different tasks and sequences. Data points are indicated as dots grouped into bins of width 5. Horizontal lines within the plots indicate 0.25, 0.50 and 0.75 quantiles"----
+reproduce_boxplot_CORRECTNESS()
+
+## ------------------------------------------------------------------------
+reproduce_lmm_CORRECTNESS()
+knitr::kable(reproduce_estimates_CORRECTNESS(), caption = "TABLE 6 Estimates of fixed effects for CORRECTNESS")
+
+## ------------------------------------------------------------------------
+knitr::kable(reproduce_hypothesis_testing_CORRECTNESS(), caption = "TABLE 7 Tests of fixed effects for CORRECTNESS")
 
 ## ----descriptive_stats_COMP----------------------------------------------
 knitr::kable(reproduce_descriptive_stats_COMPLETENESS())
 
-## ---- fig.cap = "Figure X"-----------------------------------------------
-  reproduce_violin_plot_COMPLETENESS()
+## ---- fig.cap = "Fig. 5. Violin plots for COMPLETENESS at both levels of Task Description Granularity. Data points are indicated as dots grouped into bins of width 5. Horizontal lines within the plots indicate 0.25, 0.50 and 0.75 quantiles."----
+reproduce_violin_plot_COMPLETENESS()
+
+## ---- fig.cap = "Fig. 6. Boxplots for COMPLETENESS shown separately on different tasks and sequences. Data points are indicated as dots grouped into bins of width 5. Horizontal lines within the plots indicate 0.25, 0.50 and 0.75 quantiles"----
+reproduce_boxplot_COMPLETENESS()
 
